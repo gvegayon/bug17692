d6fe7ce2940e38d68584938418ea07bee23205db
George G. Vega Yon <g.vegayon@gmail.com>
2020-01-15 11:39:26 -0800

    Adding tinytest

--- inst/tinytest/test_bug17692.R
+++ inst/tinytest/test_bug17692.R
@@ -1,12 +1,12 @@
-library(bug17692)
 
+# Placeholder with simple test
 cl <- makePSOCKcluster(2)
 ans0 <- sim_pi(100, cl)
-stopCluster(cl) # This is OK
+expect_silent(stopCluster(cl)) # This is OK
 
 # Fork is not
 if (.Platform$OS.type == "unix") {
   cl <- makeForkCluster(2)
   ans0 <- sim_pi(100, cl)
-  stopCluster(cl) # Produces errors catched by valgrind
+  expect_silent(stopCluster(cl)) # Produces errors catched by valgrind
 }
